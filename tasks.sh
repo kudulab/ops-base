d6c21b329404d767effddfe00754b2fbfbda2117
Tomasz Setkowski <tom@ai-traders.com>
2019-04-20 14:57:04 +0000

    added itest with releaser #17555

--- tasks.sh
+++ tasks.sh
@@ -27,10 +27,25 @@ case "${command}" in
   test)
     distribution=$2
     if [ -z "$distribution" ]; then
-      echo "must specify distribution to build"
+      echo "must specify distribution to test"
       exit 1
     fi
     docker run --privileged --rm --volume $PROJECT_DIR/test:/test ops-base:$distribution './test/test.sh'
+    ;;
+  itest)
+    distribution=$2
+    component=$3
+    if [ -z "$distribution" ]; then
+      echo "must specify distribution to test"
+      exit 1
+    fi
+    if [ -z "$component" ]; then
+      echo "must specify component to test with"
+      exit 1
+    fi
+    component_dir="$(readlink -f $PROJECT_DIR/../$component)"
+    echo "Testing with $component at $component_dir"
+    docker run --privileged --rm --volume $component_dir:/test ops-base:$distribution bash -c 'cd ./test/ && ./tasks test'
     ;;
   *)
       echo "Invalid command: '${command}'"
