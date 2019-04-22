66c047167ad299ed32b8ca4984de87d4a7eea000
Tomasz Setkowski <tom@ai-traders.com>
2019-04-22 16:42:51 +0000

    release task, release 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
-### 0.2.0 - Unreleased (2018-Apr-16)
+### 0.2.0 (2019-Apr-22)
 
  * Making new specs \#17556
  * merged script from workstation primary
+ * testing releaser, secret-ops, docker-ops after each build
 
 ### 0.1.0 (2018-Dec-07)
 
--- tasks.sh
+++ tasks.sh
@@ -3,7 +3,10 @@ set -e
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-SECRET_OPS_VERSION="0.6.1"
+SECRET_OPS_VERSION="0.6.2"
+RELEASER_VERSION="2.0.0"
+
+RELEASER_FILE="ops/releaser-${RELEASER_VERSION}"
 SECRET_OPS_FILE="ops/secret-ops"
 SECRET_OPS_TAR_FILE="ops/secret-ops-${SECRET_OPS_VERSION}.tar.gz"
 
@@ -13,6 +16,11 @@ if [[ ! -f $SECRET_OPS_TAR_FILE ]];then
   tar -xf $SECRET_OPS_TAR_FILE -C ops
 fi
 source $SECRET_OPS_FILE
+
+if [[ ! -f $RELEASER_FILE ]];then
+  wget --quiet -O $RELEASER_FILE https://github.com/kudulab/releaser/releases/download/${RELEASER_VERSION}/releaser
+fi
+source $RELEASER_FILE
 
 command="$1"
 case "${command}" in
@@ -54,6 +62,18 @@ case "${command}" in
     component_dir="$(readlink -f $PROJECT_DIR/../$component)"
     echo "Testing with $component at $component_dir"
     docker run --privileged -e VAULT_TOKEN=$VAULT_TOKEN -t --rm --volume $component_dir:/test ops-base:$distribution bash -c 'sudo service docker start && cd ./test/ && ./tasks test'
+    ;;
+  release)
+    # conditional release
+    git fetch origin
+    current_commit_git_tags=$(git tag -l --points-at HEAD)
+    if [[ "${current_commit_git_tags}" != "" ]];then
+      releaser::log_info "Current commit is already tagged, skipping code release"
+    else
+      releaser::log_info "Current commit has no tags, starting code release..."
+      releaser::verify_version_for_release
+      releaser::git_tag_from_changelog
+    fi
     ;;
   generate_vault_token)
     vault_token=$(vault token create -ttl=48h -policy=gocd -field token -metadata gocd_renew=true)
